88c0381ebbdd22005ddc48a3e09110087805d574
yu-retel <dbdudals100@naver.com>
2022-11-18 18:46:07 +0900

    refactor(Validtion): 테스트 결과에 따른 코드수정
    - 다리길이를 문자열로 받아와서 길이를 측정 -> 다리길이를 넘버화 시켜서 비교

--- src/Validtion.js
+++ src/Validtion.js
@@ -1,13 +1,18 @@
 class BridgeValidtion {
 
-static validateBridge(bridge) {
-  if(bridge.length < 3 || bridge.length > 20) {
-    throw new Error('[ERROR] 3에서 20까지의 숫자만 입력하셔야 합니다.')
+  static validateBridge(bridge) {
+    if(Number(bridge) < 3 || Number(bridge) > 20) {
+      throw new Error('[ERROR] 3에서 20까지의 숫자만 입력하셔야 합니다.')
+    }
+    if(Number.isNaN(Number(bridge))) {
+      throw new Error('[ERROR] 숫자만 입력하셔야 합니다.')
+    }
   }
-  if(Number.isNaN(Number(bridge))) {
-    throw new Error('[ERROR] 숫자만 입력하셔야 합니다.')
+  static validateSelect(select) {
+    if(select.length !== 1) {
+      throw new Error('[ERROR] 한자리의 U와 D를 입력하셔야 합니다.')
+    }
   }
-}
 }
 
 module.exports = BridgeValidtion;
