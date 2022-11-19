04212ac0180238c517bf61126a696ebcb20239da
yu-retel <dbdudals100@naver.com>
2022-11-19 14:31:14 +0900

    feat: 비교값을 받아 양식대로 출력하는 기능 구현

--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -1,4 +1,7 @@
 const { Console } = require("@woowacourse/mission-utils");
+const BridgeGame = require('../Model/BridgeGame');
+
+const b = new BridgeGame();
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -12,7 +15,16 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+
+  length(size) {
+    b.bridgeLengthInput(size);
+  },
+
+  printMap(input) {
+    b.move(input);
+    Console.print(`[${b.userStatus[0].join('|')}]`);
+    Console.print(`[${b.userStatus[1].join('|')}]`);
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
