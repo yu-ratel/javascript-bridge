0abbbf96639b26fdeec0a6f3428939c701849946
yu-retel <dbdudals100@naver.com>
2022-11-20 14:04:57 +0900

    feat: 종료 기능 구현

--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -51,14 +51,35 @@ const OutputView = {
 
     return true;
   },
+
+  totalGameText() {
+    if(game.answer()) {
+      return '\n최종 게임 결과';
+    }
+
+    return '최종 게임 결과';
+  },
+
+  answerText() {
+    if(game.answer()) {
+      return '성공';
+    }
+
+    return '실패';
+  },
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
    printResult() {
-    
-   }
+    Console.print(`${this.totalGameText()}`)
+    Console.print(`[${game.userStatus[0].join('|')}]`);
+    Console.print(`[${game.userStatus[1].join('|')}]\n`);
+    Console.print(`게임 성공 여부: ${this.answerText()}`);
+    Console.print(`총 시도한 횟수: ${game.totolCount}`);
+    Console.close();
+   },
 };
 
 module.exports = OutputView;
