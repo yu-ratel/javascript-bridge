d5dc06d51cca6794486a693837588a2e35b425f3
yu-retel <dbdudals100@naver.com>
2022-12-08 15:01:15 +0900

    feat: 다리를 생성하는 기능 구현

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -7,7 +7,13 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+  makeBridge(size, generateRandomNumber) {
+  const bridge = (number) => ['D', 'U'][number];
+
+  return Array.from({length: Number(size)}, generateRandomNumber)
+  .map(bridge,
+    );
+  },
 };
 
 module.exports = BridgeMaker;
