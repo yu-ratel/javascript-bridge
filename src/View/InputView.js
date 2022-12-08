f98cc28ccfede9afec0afb683f79242404e3035e
yu-retel <dbdudals100@naver.com>
2022-12-08 15:07:37 +0900

    feat: 플레이어가 이동할 칸을 입력받는 기능구현

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -7,13 +7,15 @@ const InputView = {
    * 다리의 길이를 입력받는다.
    */
   readBridgeSize() {
-    Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)\n' , callback);
+    Console.readLine('다리의 길이를 입력해주세요.\n' , callback);
   },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래:D)\n' , callback);
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
